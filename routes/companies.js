53c7c5c291f79cb09e86ae474fc0bd2aa5c1a986
RalphsCode <RalphsCode@gmail.com>
2024-10-14 22:24:19 -0700

    PUT route added.

--- routes/companies.js
+++ routes/companies.js
@@ -27,9 +27,38 @@ router.get("/:code", async function (req, res, next) {
    return next(err); } });
 
 
+// Update a company
+router.put("/:code", async function (req, res, next) {
+    try { 
+        const updateCode = req.params.code;
+        const existingData = await db.query( 'SELECT name, description FROM companies WHERE code = $1', [updateCode]);
+        // If Company Code can't be located
+        if (existingData.rows.length === 0) {
+            return next(new ExpressError('Unable to Update; Company not found', 404));
+          }
+        // If the Company Name is updated
+        if (req.body.name) {
+            db.query( 'UPDATE companies SET name = $1  WHERE code = $2', [req.body.name, updateCode])
+        };
+        // If the Company Description is updated
+        if (req.body.description) {
+            db.query( 'UPDATE companies SET description = $1 WHERE code = $2', [req.body.description, updateCode])
+        };
+        // Get the updated company from the database
+        const updatedCompany = await db.query(
+            'SELECT * FROM companies WHERE code = $1',
+            [updateCode]
+          );
+    // Return
+   return res.json({company: updatedCompany.rows[0]}); 
+   } catch (err) { 
+   return next(err); } });
+
+
 // Add a company
 router.post("/", async function (req, res, next) {
     try { 
+
     const newCode = req.body.code;
     const newName = req.body.name;
     const newDesc = req.body.desc || 'none';
