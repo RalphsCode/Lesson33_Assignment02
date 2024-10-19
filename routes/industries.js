0968d3815eb92ebd4f937fa5f777ce7275ad8908
RalphsCode <RalphsCode@gmail.com>
2024-10-18 23:07:23 -0700

    Working on the M2M route.

--- routes/industries.js
+++ routes/industries.js
@@ -2,6 +2,7 @@ const express = require("express");    // require express just to get router
 const router = new express.Router();
 const db = require("../db")
 const ExpressError = require("../expressError");
+const { name } = require("../app");
 
 
 // Return industry & Companies
@@ -17,12 +18,14 @@ router.get("/", async function (req, res, next) {
         console.log(industryList);
 
         // // Get the companies in an industry
-        for (indCode in industryList) {
-            
-        }
-        // const companyResults = await db.query( 'SELECT i.code, i.industry, c.name FROM industries AS i LEFT JOIN company_industry AS c_i ON i.code = c_i.industry_code LEFT JOIN companies AS c ON c.code = c_i.company_code WHERE i.code = $1', [industry_code]); 
+        const companyResults = []
+        for (indCode of industryList) {
+            const results = await db.query( 'SELECT c.name FROM industries AS i LEFT JOIN company_industry AS c_i ON i.code = c_i.industry_code LEFT JOIN companies AS c ON c.code = c_i.company_code WHERE i.code = $1', [indCode]);
 
-        return res.json({industries: industryResults.rows}); 
+            companyResults.push(results.rows);
+        }  // END for loop...
+        
+        return res.json({industries: industryResults.rows, companies: companyResults}); 
    
    } catch (err) { 
    return next(err); } });
