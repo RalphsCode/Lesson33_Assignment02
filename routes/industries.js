c761ccfc556b4f65f587463efb4866c524bcf55c
RalphsCode <RalphsCode@gmail.com>
2024-10-19 22:50:56 -0700

    Industries route is functional.

--- routes/industries.js
+++ routes/industries.js
@@ -5,36 +5,40 @@ const ExpressError = require("../expressError");
 const { name } = require("../app");
 
 
-// Return industry & Companies
+// Return details for all industries and related companies
 router.get("/", async function (req, res, next) {
     try { 
         // Get all industry codes
-        const industryResults = await db.query( 'SELECT code from industries');
-        // Create a list of industry codes
-        const industryList = [];
-        for (x of industryResults.rows) {
-            industryList.push(x.code);
-        }
-        console.log(industryList);
+        const industryResults = await db.query( 'SELECT * from industries');
 
-        // // Get the companies in an industry
-        const companyResults = []
-        for (indCode of industryList) {
-            const results = await db.query( 'SELECT c.name FROM industries AS i LEFT JOIN company_industry AS c_i ON i.code = c_i.industry_code LEFT JOIN companies AS c ON c.code = c_i.company_code WHERE i.code = $1', [indCode]);
+        // Object to store Industries
+        const industries = {};
 
-            companyResults.push(results.rows);
-        }  // END for loop...
-        
-        return res.json({industries: industryResults.rows, companies: companyResults}); 
+        // Loop over each industry and store its details
+        for (ind of industryResults.rows) {
+            // Define a key for the dictionary entry
+            const key = ind.code;
+            // Get the company code(s) for companies in the looped industry
+            const comps = await db.query( 'SELECT c.code FROM company_industry AS c_i LEFT JOIN industries AS i ON i.code = c_i.industry_code LEFT JOIN companies AS c ON c.code = c_i.company_code WHERE i.code = $1', [ind.code]);
+            // Add the found companies to an array
+            let companyResults = [];
+            if (comps.rows.length !== 0){                
+                for (c in comps.rows) {
+                    companyResults.push(comps.rows[c].code);
+                }  // END for
+            }  // END if
+
+            // Add the industry info to the industries object
+            industries[key] = {"code": ind.code, "name": ind.industry, "companies": companyResults};
+
+        }  // END for loop
+
+        // Return the Industries Object
+        return res.json({industries}); 
    
-   } catch (err) { 
-   return next(err); } });
+    } catch (err) { 
+    return next(err); } });
+//  END Return all Industries Route
 
 
-   module.exports = router; 
-
-   /*
-
-   ;
-
-   */
+module.exports = router; 
